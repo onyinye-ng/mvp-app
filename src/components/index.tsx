d5576527cea35f6ddccec4cbfefb418f3c80c427
Ebuka Odini <ebukaodini@gmail.com>
2022-10-28 17:37:05 +0100

    refactor: updated components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,5 @@
 export { OnboardingWrapper } from "./layouts/OnboardingWrapper"
-export { DashboardWrapper } from "./layouts/DashboardWrapper"
+// export { DashboardWrapper } from "./layouts/DashboardWrapper"
 export { Button, IconButton, LinkButton, TextButton } from "./forms/FormButtons"
 export { IconLink, TextLink } from "./forms/Links"
 export { Input, PrefixInput, CheckboxInput, TextareaInput, Label } from "./forms/Inputs"
